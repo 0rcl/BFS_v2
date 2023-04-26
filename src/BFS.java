3afb21fc87e4bba69803400af322e46d24174cb2
0rcl <105236460+0rcl@users.noreply.github.com>
2023-04-26 08:10:26 -0700

    Update BFS.java

--- src/BFS.java
+++ src/BFS.java
@@ -1,35 +1,29 @@
 import java.util.*;
 public class BFS {
-    /**
-     * Durchläuft den Graphen mit der Breitensuche von einem Startknoten aus
-     * und gibt die besuchten Knoten aus.
-     *
-     * @param graph die Adjazenzmatrix, die den Graphen repräsentiert
-     * @param start der Startknoten, von dem aus die Breitensuche beginnen soll
-     */
+  //1
     public static void breadthFirstSearch(int[][] graph, int start) {
-        // Markiere alle Knoten als unbesucht
+        //2
         boolean[] visited = new boolean[graph.length];
 
-        // Erstelle eine Warteschlange, um die zu besuchenden Knoten zu speichern
+        // 3
         Queue<Integer> queue = new LinkedList<>();
 
-        // Markiere den Startknoten als besucht und füge ihn in die Warteschlange ein
+        //4
         visited[start] = true;
         queue.offer(start);
 
-        // Solange die Warteschlange nicht leer ist
+        // 5
         while (!queue.isEmpty()) {
-            // Entnehme den nächsten Knoten aus der Warteschlange
+            // 6
             int current = queue.poll();
-            // Gib den besuchten Knoten aus
+            // 7
             System.out.print(current + " ");
 
-            // Durchlaufe alle Nachbarn des aktuellen Knotens
+            // 8
             for (int neighbor = 0; neighbor < graph[current].length; neighbor++) {
-                // Wenn der Nachbar eine Verbindung zum aktuellen Knoten hat und noch nicht besucht wurde
+                //9
                 if (graph[current][neighbor] == 1 && !visited[neighbor]) {
-                    // Markiere ihn als besucht und füge ihn in die Warteschlange ein
+                    // 10
                     visited[neighbor] = true;
                     queue.offer(neighbor);
                 }
@@ -38,7 +32,7 @@ public class BFS {
     }
 
     public static void main(String[] args) {
-        // Erstelle eine Adjazenzmatrix für einen Graphen mit 6 Knoten
+        // 11
         int[][] graph = new int[][]{
                 {0, 1, 1, 0, 0, 0},
                 {1, 0, 1, 0, 0, 0},
@@ -48,7 +42,7 @@ public class BFS {
                 {0, 0, 0, 0, 1, 0}
         };
 
-        // Führe die Breitensuche von Knoten 2 aus und gib die besuchten Knoten aus
+        // 12
         System.out.print("Breitensuche: ");
         breadthFirstSearch(graph, 2);
     }
